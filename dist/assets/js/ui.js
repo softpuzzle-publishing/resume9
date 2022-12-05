67fce8c3d3ad5e9c7290d37e4917fdd89043cf60
park <rmstn133@gmail.com>
2022-12-05 18:41:58 +0900

    채팅앱 수정

--- dist/assets/js/ui.js
+++ dist/assets/js/ui.js
@@ -39,7 +39,19 @@ $(document).ready(function () {
     $(this).toggleClass("active").siblings().removeClass("active");
   }); // datepicker
 
-  $("#datepicker").datepicker();
+  $("#datepicker").datepicker({
+    changeMonth: true,
+    changeYear: true,
+    nextText: "다음 달",
+    prevText: "이전 달",
+    currentText: "오늘 날짜",
+    closeText: "닫기",
+    dateFormat: "yy-mm-dd",
+    showMonthAfterYear: true,
+    dayNamesMin: ["월", "화", "수", "목", "금", "토", "일"],
+    monthNamesShort: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
+  });
+  $("#datepicker .highlight a").tooltip("disable");
   $(".more-dot").click(function () {
     $(this).addClass("active");
   });
@@ -48,4 +60,11 @@ $(document).ready(function () {
       $(".more-dot").removeClass("active");
     }
   });
+}); // 채팅창 모바일
+
+$(".message-list .list li").click(function () {
+  $(".message-content").addClass("active");
+});
+$(".opponent .back").click(function () {
+  $(".message-content").removeClass("active");
 });
